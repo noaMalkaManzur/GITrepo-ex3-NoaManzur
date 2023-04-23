f48839c78e92bf52b1e355bec55823558fd88373
noaMalkaManzur <noamanzur1998@gmail.com>
2023-04-23 11:52:01 +0300

    changing print of Noa Malka Manzur

--- NoaMalkaManzur.java
+++ NoaMalkaManzur.java
@@ -2,7 +2,7 @@ public class NoaMalkaManzur{
 
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Noa Malka Manzur  ..."+i);
+			System.out.println("Noa111  ..."+i);
 		}
 	}
 }
